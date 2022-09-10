0a414c03a8fe030c5687786c9f29f42342518cad
DanielaOrbes <daniiorbes15@gmail.com>
2022-09-09 22:03:42 -0300

    APIs

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const methodOverride = require('method-override');
+const app = express();
+
+
+
+const indexRouter = require('./routes/index');
+const moviesRoutes = require('./routes/moviesRoutes');
+const genresRoutes = require('./routes/genresRoutes');
+const genresRoutesApi = require('./routes/api/genresRoutes');
+const moviesRoutesApi = require('./routes/api/movieRoutes');
+
+
+app.set('views', path.resolve(__dirname, './views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.resolve(__dirname, '../public')));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+app.use(methodOverride('_method'));
+
+app.use('/', indexRouter);
+app.use(moviesRoutes);
+app.use(genresRoutes);
+app.use('/api/genres', genresRoutesApi)
+app.use('/api/movies', moviesRoutesApi)
+
+
+
+
+app.listen('3020', () => console.log('Servidor corriendo en http://localhost3020'));
